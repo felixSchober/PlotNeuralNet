362b25ad694519c7827ff91159ad22e24b6d8bbb
Felix Schober <github@felix-schober.com>
2019-04-28 01:18:22 +0200

    use existing pr to make the tool work on macs

--- tikzmake.sh
+++ tikzmake.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 
-python $1.py 
+python3 $1.py 
 pdflatex $1.tex
 
 rm *.aux *.log *.vscodeLog
 rm *.tex
 
-xdg-open $1.pdf
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	    open $1.pdf
+else
+	xdg-open $1.pdf
+fi
 
